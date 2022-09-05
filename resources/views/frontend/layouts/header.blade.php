b3ddbe4d01fb49ccaa4b9be4a4940d4757f96b37
hardikhunani <hunaniinfotechhardik@gmail.com>
2022-09-05 14:33:01 +0530

    about Page Done

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="./images/newlogo.png"></a>
+                <a class="navbar-brand" href="/"><img src="./images/newlogo.png"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav m-auto">
@@ -30,11 +30,11 @@
                                 <li><a class="dropdown-item text-uppercase" href="#">Copy Trading</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle text-light text-uppercase" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle text-light text-uppercase" href="/about.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         About
                     </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item text-uppercase" href="#">About Us</a></li>
+                                <li><a class="dropdown-item text-uppercase" href="{{Route('aboutus')}}">About Us</a></li>
                                 <li><a class="dropdown-item text-uppercase" href="#">Fees and Conditions</a></li>
                             </ul>
                         </li>
